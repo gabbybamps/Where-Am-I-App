bd1562dccd07fcad2e0188e9537290472ce95588
gabbybamps <gabbybamps@gmail.com>
2021-08-16 07:42:44 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Where Am I App
+
+A web app which uses the geolocation and reverse geocoding APIs to fetch the location of the user at the press of a button.
+It then renders features(flag, population etc.) of the country of location in the DOM.
---
+++ script.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText(`beforeend`, msg);
+  countriesContainer.style.opacity = 1;
+};
+
+const renderCountry = function (data, className = ``) {
+  const html = `
+        <article class="country ${className}">
+              <img class="country__img" src="${data.flag}" />
+              <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(
+                  +data.population / 1000000
+                ).toFixed(1)} M people</p>
+                <p class="country__row"><span>🗣️</span>${
+                  data.languages[0].name
+                }</p>
+                <p class="country__row"><span>💰</span>${
+                  data.currencies[0].name
+                }</p>
+              </div>
+            </article>
+    
+        `;
+
+  countriesContainer.insertAdjacentHTML(`beforeend`, html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+const whereAmI = async function () {
+  try {
+    // Geolocation
+    const pos = await getPosition();
+
+    const { latitude: lat, longitude: lng } = pos.coords;
+
+    // Reverse Geocoding
+    const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+
+    if (!resGeo.ok) throw new Error(`Problem getting location data`);
+
+    const dataGeo = await resGeo.json();
+
+    // Country data
+    const res = await fetch(
+      `https://restcountries.eu/rest/v2/name/${dataGeo.country}`
+    );
+
+    if (!res.ok) throw new Error(`Problem getting country`);
+
+    const data = await res.json();
+    renderCountry(data[0]);
+  } catch (err) {
+    renderError(` 🛑  ${err.message}`);
+  }
+};
+
+btn.addEventListener(`click`, whereAmI);
